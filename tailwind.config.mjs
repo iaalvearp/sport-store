cb2429328194f55cd7ce991f3a96df3058873d28
AlPes <iaalvearp@gmail.com>
2024-03-12 07:35:43 -0500

    Nav and Hero section done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,11 @@ import animations from "tailwindcss-animated";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ["vanilla", "sans-serif"], // 'micross' es el nombre de la fuente
+      },
+    },
   },
   plugins: [animations],
 };
